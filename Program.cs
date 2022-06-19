5401392d399597f41d0051da35b22c5671db3242
Akindats1 <akindats1@gmail.com>
2022-06-19 08:19:16 +0100

    FreeApp

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FreeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int c = 0;
+            while (c <= 20)
+            {
+              if(c % 3 == 0);
+              {
+                sum += c;
+              }
+            }
+            Console.WriteLine($"The sum of the numbers are:{sum}");
+        }
+    }
+}
